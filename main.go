0af63cedbc73df2824d78fb158dd8ac8a121d675
Andrew Snodgrass <git@pointlander.info>
2023-09-12 21:57:52 -0500

    Random sampling

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"sort"
+	"math/rand"
 
 	"github.com/bugra/kmeans"
 	"github.com/pointlander/datum/iris"
@@ -17,11 +17,6 @@ import (
 func MNIST() {
 }
 
-// SelfAware is the self aware mode
-func SelfAware(datum iris.Datum, fisher Matrix) {
-
-}
-
 var (
 	// MNIST MNIST mode
 	FlagMNIST = flag.Bool("mnist", false, "mnist mode")
@@ -29,6 +24,7 @@ var (
 
 func main() {
 	flag.Parse()
+	rnd := rand.New(rand.NewSource(1))
 
 	if *FlagMNIST {
 		MNIST()
@@ -40,43 +36,75 @@ func main() {
 		panic(err)
 	}
 
-	fisher := NewMatrix(0, 4, len(datum.Fisher))
-	for _, embedding := range datum.Fisher {
-		fisher.Data = append(fisher.Data, embedding.Measures...)
+	sample := func() (x [150][]int) {
+		fisher := NewMatrix(0, 4, len(datum.Fisher))
+		for _, embedding := range datum.Fisher {
+			for _, measure := range embedding.Measures {
+				fisher.Data = append(fisher.Data, measure+rnd.NormFloat64()*0.1)
+			}
+		}
+
+		units := Normalize(fisher)
+		projected := PCA(units)
+		embedded := SelfAttention(projected, projected, projected)
+
+		type Vector struct {
+			Index int
+			Value []float64
+		}
+		vectors := make([]Vector, 0, len(embedded.Data))
+		for i := 0; i < embedded.Rows; i++ {
+			v := embedded.Data[i*embedded.Cols : i*embedded.Cols+embedded.Cols]
+			vectors = append(vectors, Vector{
+				Index: i,
+				Value: v,
+			})
+		}
+
+		rawData := make([][]float64, len(vectors))
+		for i, v := range vectors {
+			rawData[i] = v.Value
+		}
+		clusters, err := kmeans.Kmeans(rawData, 3, kmeans.SquaredEuclideanDistance, -1)
+		if err != nil {
+			panic(err)
+		}
+		for i := range x {
+			x[i] = make([]int, 150)
+			target := clusters[i]
+			for j, v := range clusters {
+				if v == target {
+					x[i][j]++
+				}
+			}
+		}
+		return x
 	}
 
-	units := Normalize(fisher)
-	projected := PCA(units)
-	embedded := SelfAttention(projected, projected, projected)
-	type Vector struct {
-		Index int
-		Value []float64
-		Label string
+	var sum [150][]int
+	for i := range sum {
+		sum[i] = make([]int, 150)
 	}
-	vectors := make([]Vector, 0, len(embedded.Data))
-	for i := 0; i < embedded.Rows; i++ {
-		v := embedded.Data[i*embedded.Cols : i*embedded.Cols+embedded.Cols]
-		vectors = append(vectors, Vector{
-			Index: i,
-			Value: v,
-			Label: datum.Fisher[i].Label,
-		})
+	for i := 0; i < 100; i++ {
+		x := sample()
+		for i := range sum {
+			for j := range sum[i] {
+				sum[i][j] += x[i][j]
+			}
+		}
 	}
-	sort.Slice(vectors, func(i, j int) bool {
-		return vectors[i].Value[0] < vectors[j].Value[0]
-	})
-	for _, v := range vectors {
-		fmt.Printf("%s %f\n", datum.Fisher[v.Index].Label, v.Value)
+
+	rawData := make([][]float64, len(sum))
+	for i, v := range sum {
+		for j := range v {
+			rawData[i] = append(rawData[i], float64(v[j]))
+		}
 	}
-	rawData := make([][]float64, len(vectors))
-	for i, v := range vectors {
-		rawData[i] = v.Value
-	}
-	clusters, err := kmeans.Kmeans(rawData, 3, kmeans.SquaredEuclideanDistance, -1)
+	clusters, err := kmeans.Kmeans(rawData, 3, kmeans.EuclideanDistance, -1)
 	if err != nil {
 		panic(err)
 	}
 	for i, v := range clusters {
-		fmt.Println(vectors[i].Label, i, v)
+		fmt.Println(datum.Fisher[i].Label, i, v)
 	}
 }
