b9aed6dd48d6cdb1ac4e6fe350ee633bdfc20c07
Andrew Snodgrass <git@pointlander.info>
2023-09-13 20:30:15 -0500

    Refactor

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"sort"
 
 	"github.com/bugra/kmeans"
 	"github.com/pointlander/datum/iris"
@@ -18,19 +19,9 @@ import (
 func MNIST() {
 }
 
-var (
-	// MNIST MNIST mode
-	FlagMNIST = flag.Bool("mnist", false, "mnist mode")
-)
-
-func main() {
-	flag.Parse()
+// Sample samples the embedding function for the iris data set
+func Sample() {
 	rnd := rand.New(rand.NewSource(1))
-
-	if *FlagMNIST {
-		MNIST()
-		return
-	}
 
 	datum, err := iris.Load()
 	if err != nil {
@@ -129,5 +120,71 @@ func main() {
 	}
 	for i, v := range clusters {
 		fmt.Println(datum.Fisher[i].Label, i, v)
+	}
+}
+
+var (
+	// MNIST MNIST mode
+	FlagMNIST = flag.Bool("mnist", false, "mnist mode")
+	// FlagSample sample mode
+	FlagSample = flag.Bool("sample", false, "sample mode")
+)
+
+func main() {
+	flag.Parse()
+
+	if *FlagMNIST {
+		MNIST()
+		return
+	}
+
+	if *FlagSample {
+		Sample()
+		return
+	}
+
+	datum, err := iris.Load()
+	if err != nil {
+		panic(err)
+	}
+
+	fisher := NewMatrix(0, 4, len(datum.Fisher))
+	for _, embedding := range datum.Fisher {
+		fisher.Data = append(fisher.Data, embedding.Measures...)
+	}
+
+	units := Normalize(fisher)
+	projected := PCA(units)
+	embedded := SelfAttention(projected, projected, projected)
+	type Vector struct {
+		Index int
+		Value []float64
+		Label string
+	}
+	vectors := make([]Vector, 0, len(embedded.Data))
+	for i := 0; i < embedded.Rows; i++ {
+		v := embedded.Data[i*embedded.Cols : i*embedded.Cols+embedded.Cols]
+		vectors = append(vectors, Vector{
+			Index: i,
+			Value: v,
+			Label: datum.Fisher[i].Label,
+		})
+	}
+	sort.Slice(vectors, func(i, j int) bool {
+		return vectors[i].Value[0] < vectors[j].Value[0]
+	})
+	for _, v := range vectors {
+		fmt.Printf("%s %f\n", datum.Fisher[v.Index].Label, v.Value)
+	}
+	rawData := make([][]float64, len(vectors))
+	for i, v := range vectors {
+		rawData[i] = v.Value
+	}
+	clusters, err := kmeans.Kmeans(rawData, 3, kmeans.SquaredEuclideanDistance, -1)
+	if err != nil {
+		panic(err)
+	}
+	for i, v := range clusters {
+		fmt.Println(vectors[i].Label, i, v)
 	}
 }
